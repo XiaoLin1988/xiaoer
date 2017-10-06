53d672b9032ebff8f657b6e9795429dc84633318
xiaolin1988 <XiaoLin_1988@hotmail.com>
2017-10-06 15:52:55 +0800

    API initial commit

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -15,7 +15,30 @@ class Api extends CI_Controller {
     }
 
     public function createShangjia() {
+        $result = array();
 
+        $time = time();
+
+        $shangjia_data = array(
+            'sj_name' => $_POST['sj_name'],
+            'sj_otime' => time(),
+            'sj_etime' => time(),
+            'sj_addr' => $_POST['sj_addr'],
+            'sj_lat' => $_POST['sj_lat'],
+            'sj_lng' => $_POST['sj_lng'],
+            'sj_ctime' => $time
+        );
+
+        $ret = $this->api->createShangjia($shangjia_data);
+        if (gettype($ret) == 'boolean') {
+            $result['status'] = 'error';
+            $result['data'] = 'Could not save into the database';
+        } else {
+            $result['status'] = 'success';
+            $result['data'] = $ret;
+        }
+
+        echo json_encode($result);
     }
 
     public function createBaoxiang() {
@@ -45,7 +68,8 @@ class Api extends CI_Controller {
         } else {
             $maijiu_data = array(
                 'mj_sj_id' => $_POST['sj_id'],
-                'mj_oyh_id' => $_POST['user_id']
+                'mj_oyh_id' => $_POST['user_id'],
+                'mj_pyh_id' => $_POST['payer_id']
             );
         }
     }
--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -9,6 +9,26 @@
 class Api_model extends CI_Model {
     public function __construct() {
         parent::__construct();
+    }
 
+    public function createShangjia($data) {
+        $columns = "";
+        $values = "";
+        foreach ($data as $key=>$value) {
+            $columns .= $key.",";
+            $values .= "'".$value."',";
+        }
+        $columns = rtrim($columns, ",");
+        $values = rtrim($values, ",");
+
+        $query = "INSERT INTO tbl_shangjia(".$columns.") VALUES(".$values.")";
+
+        $res = $this->db->query($query);
+
+        if ($res) {
+            return $this->db->insert_id();
+        } else {
+            return $res;
+        }
     }
 }
