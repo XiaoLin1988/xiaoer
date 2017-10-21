c65b1edff2c43274c0943371c168433fc46738a6
xiaolin1988 <XiaoLin_1988@hotmail.com>
2017-10-22 00:41:59 +0800

    API initial commit

--- application/controllers/api/Common.php
+++ application/controllers/api/Common.php
@@ -10,7 +10,7 @@ class Common extends MY_Controller {
 
     public function imageUpload() {
         $result = array();
-        $rootpath = 'uploads/';
+        $file = 'uploads/';
 
         if (!isset($_FILES['image'])) {
             $result['status'] = false;
@@ -20,15 +20,29 @@ class Common extends MY_Controller {
             $result['data'] = 'Please select your image type';
         } else {
             if ($_POST['type'] == 1) {
-                $rootpath .= 'shangjia/';
+                $file .= 'shangjia/';
             } else if ($_POST['type'] == 2) {
-                $rootpath .= 'jiu/';
+                $file .= 'jiu/';
             } else if ($_POST['type'] == 3 or $_POST['type'] == 4) {
-                $rootpath .= 'avatar/';
+                $file .= 'avatar/';
             }
 
-            $time = time();
+            $file .= time().'.png';
+            if(file_exists($file)) {
+                chmod($file, 0755);
+                unlink($file);
+            }
 
+            $upFile = $_FILES['image'];
+
+            $ret = move_uploaded_file($upFile['tmp_name'], $file);
+            if ($ret == TRUE) {
+                $result['status'] = true;
+                $result['data'] = $file;
+            } else {
+                $result['status'] = false;
+                $result['data'] = 'Couldn\'t copy image, try again';
+            }
         }
 
         echo json_encode($result);
