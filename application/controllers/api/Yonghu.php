e8f385f59630ee64b512d292ad1df08983ed25f1
xiaohan <32392745+saeedmohamed451@users.noreply.github.com>
2017-10-22 15:53:51 +0800

    yonghe

--- application/controllers/api/Yonghu.php
+++ application/controllers/api/Yonghu.php
@@ -8,9 +8,56 @@
  */
 class Yonghu extends MY_Controller
 {
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
+        $this->load->model('Yonghu_model', 'yonghu');
     }
 
+    public function create() {
+        $result = array();
+
+        $data = array(
+            'yh_name' => $_POST['name'],
+            'yh_openId' => $_POST['openId'],
+            'yh_phone' => $_POST['phone'],
+            'yh_pwd' => $_POST['password'],
+            'yh_balance' => 0,
+            'yh_sj_id' => $_POST['shangjiaId'],
+            'yh_rlat' => 0.0,
+            'yh_rlng' => 0.0,
+            'yh_raddr' => "",
+            'yh_ctime' => time(),
+            'yh_utime' => time(),
+            'yh_df' => 0
+
+        );
+
+        $ret = $this->yonghu->create($data);
+        if (gettype($ret) == "boolean") {
+            $result['status'] = false;
+            $result['data'] = "db error";
+        } else {
+            $result['status'] = true;
+            $result['data'] = $ret;
+        }
+
+        echo json_encode($result);
+    }
+
+    public function update() {
+        $result = array();
+
+        $data = array(
+            'yh_rlat' => $_POST['rlat'],
+            'yh_rlng' => $_POST['rlng'],
+            'yh_raddr' => $_POST['raddr']
+        );
+
+        $ret = $this->yonghu->update($data, $_POST['id']);
+
+        $result['status'] = $ret;
+        $result['data'] = 'success';
+
+        echo json_encode($result);
+    }
 }
---
+++ application/models/Yonghu_model.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: macmini
+ * Date: 10/22/17
+ * Time: 3:23 PM
+ */
+class Yonghu_model extends CI_Model{
+
+    public function create($data) {
+        $columns = "";
+        $values = "";
+        foreach ($data as $key=>$value) {
+            $columns .= $key.",";
+            $values .= "'".$value."',";
+        }
+        $columns = rtrim($columns, ",");
+        $values = rtrim($values, ",");
+
+        $query = "INSERT INTO tbl_yonghu(".$columns.") VALUES(".$values.")";
+
+        $res = $this->db->query($query);
+
+        if ($res) {
+            return $this->db->insert_id();
+        } else {
+            return $res;
+        }
+    }
+
+    public function update($data, $id) {
+        $sets = "";
+        foreach ($data as $key=>$value) {
+            $sets .= $key.'="'.$value.'",';
+        }
+
+        $sets = rtrim($sets, ",");
+
+        $res = $this->db->query("UPDATE tbl_yonghu SET ".$sets." WHERE yh_id={$id}");
+
+        return $res;
+    }
+
+
+}
