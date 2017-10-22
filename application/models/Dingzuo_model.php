744ff502fa8f25fe6408ae136243dfbf3f5690b5
xiaohan <32392745+saeedmohamed451@users.noreply.github.com>
2017-10-22 17:56:34 +0800

    dingzuo

--- application/models/Dingzuo_model.php
+++ application/models/Dingzuo_model.php
@@ -2,10 +2,31 @@
 
 /**
  * Created by PhpStorm.
- * User: emerald
- * Date: 10/22/2017
- * Time: 5:09 PM
+ * User: macmini
+ * Date: 10/22/17
+ * Time: 5:49 PM
  */
 class Dingzuo_model extends CI_Model{
 
+    public function create($data) {
+        $columns = "";
+        $values = "";
+        foreach ($data as $key=>$value) {
+            $columns .= $key.",";
+            $values .= "'".$value."',";
+        }
+        $columns = rtrim($columns, ",");
+        $values = rtrim($values, ",");
+
+        $query = "INSERT INTO tbl_baoxiang(".$columns.") VALUES(".$values.")";
+
+        $res = $this->db->query($query);
+
+        if ($res) {
+            return $this->db->insert_id();
+        } else {
+            return $res;
+        }
+
+    }
 }
